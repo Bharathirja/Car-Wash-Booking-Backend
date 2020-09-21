112c0a9457c6ce32b891b929e84f8252a2427c1b
Bharathiraja SS <54929507+BharathirajaSS@users.noreply.github.com>
2020-09-21 14:28:36 +0530

    Changing url and requirement text file

--- safeShop/merchant/urls.py
+++ safeShop/merchant/urls.py
@@ -15,12 +15,12 @@ router.register(r'customerprofile', CustomerViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    re_path('^validate_phone/',ValidatePhoneSendOTP.as_view()),
-    re_path('^validate_otp/',ValidateOTP.as_view()),
-    re_path('^register/',Register.as_view()),
-    re_path('^login/',LoginAPI.as_view()),
-    re_path('^logout/',LogoutView.as_view()),
-    re_path('^logout/$',knox_views.LogoutView.as_view()),
+    path('validate_phone/',ValidatePhoneSendOTP.as_view()),
+    path('validate_otp/',ValidateOTP.as_view()),
+    path('register/',Register.as_view()),
+    path('login/',LoginAPI.as_view()),
+    path('logout/',LogoutView.as_view()),
+    path('logout/$',knox_views.LogoutView.as_view()),
     # re_path('^customerprofile/',CustomerProfile.as_view())
 
 
