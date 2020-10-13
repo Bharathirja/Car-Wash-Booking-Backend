e5d7a99cbfb51b6f51e9b7094edf6a50bf4ee7d6
Bharathiraja SS <bharathi.selvaraj08@gmail.com>
2020-10-13 14:00:14 +0530

    car wash

--- carWash/carWash/settings.py
+++ carWash/carWash/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'safeShop.urls'
+ROOT_URLCONF = 'carWash.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'safeShop.wsgi.application'
+WSGI_APPLICATION = 'carWash.wsgi.application'
 
 
 # Database
